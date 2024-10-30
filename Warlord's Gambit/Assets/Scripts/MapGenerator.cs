85c668144944d7ba26c3e29b5db7597c9daf7ae8
Samin2o35 <saminmohammed65@gmail.com>
2024-10-30 15:11:29 +0600

    Map Generation Rework 1st phase
    Beach tile variants now spawn according to neighbor tile variant

--- Warlord's Gambit/Assets/Scripts/MapGenerator.cs
+++ Warlord's Gambit/Assets/Scripts/MapGenerator.cs
@@ -1,87 +1,67 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class MapGenerator : MonoBehaviour
+public class TerrainGenerator : MonoBehaviour
 {
-    public Tilemap baseTilemap;
-    public Tilemap decorationTilemap;
-
-    // Tile references
+    public Tilemap tilemap;
+    public TileBase beachCenter;
+    public TileBase beachTopEdge, beachBottomEdge, beachLeftEdge, beachRightEdge;
+    public TileBase beachTopLeftCorner, beachTopRightCorner, beachBottomLeftCorner, beachBottomRightCorner;
+    
     public TileBase waterTile;
-    public TileBase grassTile;
-    public TileBase hillTile;
-
-    // Map setup
-    public int mapWidth = 100;
-    public int mapHeight = 100;
-    public float noiseScale = 0.1f;
-
-    // Animated decorations
-    public GameObject foamPrefab;
-    public GameObject[] rockPrefabs;
+    public int mapWidth;
+    public int mapHeight;
+    public float scale = 0.1f;
 
     void Start()
     {
-        GenerateMap();
+        GenerateTerrain();
     }
 
-    void GenerateMap()
+    void GenerateTerrain()
     {
+        bool[,] terrainMap = new bool[mapWidth, mapHeight];
+
+        // First, generate a noise-based terrain layout
         for (int x = 0; x < mapWidth; x++)
         {
             for (int y = 0; y < mapHeight; y++)
             {
-                // Generate noise value for this tile
-                float noiseValue = Mathf.PerlinNoise(x * noiseScale, y * noiseScale);
+                float perlinValue = Mathf.PerlinNoise(x * scale, y * scale);
+                terrainMap[x, y] = perlinValue > 0.5f; // True for  beach, False for water
+            }
+        }
 
-                // Set base tile based on noise value
-                if (noiseValue < 0.3f)
+        // Place tiles based on terrain layout and surrounding tiles
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                if (terrainMap[x, y])
                 {
-                    baseTilemap.SetTile(new Vector3Int(x, y, 0), waterTile);
+                    // Check surroundings to determine the type of grass tile
+                    bool top = y + 1 < mapHeight && terrainMap[x, y + 1];
+                    bool bottom = y - 1 >= 0 && terrainMap[x, y - 1];
+                    bool left = x - 1 >= 0 && terrainMap[x - 1, y];
+                    bool right = x + 1 < mapWidth && terrainMap[x + 1, y];
 
-                    // Randomly place rocks in water
-                    if (Random.value < 0.1f)
-                    {
-                        GameObject rockPrefab = rockPrefabs[Random.Range(0, rockPrefabs.Length)];
-                        Instantiate(rockPrefab, new Vector3(x, y, 0), Quaternion.identity);
-                    }
-                }
-                else if (noiseValue < 0.6f)
-                {
-                    baseTilemap.SetTile(new Vector3Int(x, y, 0), grassTile);
+                    TileBase tileToPlace = beachCenter;
+
+                    if (!top && !left) tileToPlace = beachTopLeftCorner;
+                    else if (!top && !right) tileToPlace = beachTopRightCorner;
+                    else if (!bottom && !left) tileToPlace = beachBottomLeftCorner;
+                    else if (!bottom && !right) tileToPlace = beachBottomRightCorner;
+                    else if (!top) tileToPlace = beachTopEdge;
+                    else if (!bottom) tileToPlace = beachBottomEdge;
+                    else if (!left) tileToPlace = beachLeftEdge;
+                    else if (!right) tileToPlace = beachRightEdge;
+
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tileToPlace);
                 }
                 else
                 {
-                    baseTilemap.SetTile(new Vector3Int(x, y, 0), hillTile);
+                    tilemap.SetTile(new Vector3Int(x, y, 0), waterTile);
                 }
-
-                // Add water foam between water and grass/hill tiles
-                AddWaterFoam(x, y, noiseValue);
-            }
-        }
-    }
-
-    void AddWaterFoam(int x, int y, float noiseValue)
-    {
-        // Check if water tile has a neighboring grass or hill tile
-        if (noiseValue < 0.3f)
-        {
-            bool hasLandNeighbor = false;
-
-            // Check the 4 neighbors (left, right, top, bottom)
-            foreach (Vector2Int offset in new Vector2Int[] { Vector2Int.left, Vector2Int.right, Vector2Int.up, Vector2Int.down })
-            {
-                float neighborNoiseValue = Mathf.PerlinNoise((x + offset.x) * noiseScale, (y + offset.y) * noiseScale);
-                if (neighborNoiseValue >= 0.3f) // If neighbor is grass or hill
-                {
-                    hasLandNeighbor = true;
-                    break;
-                }
-            }
-
-            if (hasLandNeighbor)
-            {
-                Instantiate(foamPrefab, new Vector3(x, y, 0), Quaternion.identity);
             }
         }
     }
